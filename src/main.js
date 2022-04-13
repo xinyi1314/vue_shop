2a7cb330f1baaf8f35e05c1cb7a64eb79d1f68d4
xinyi1314 <424392293@qq.com>
2022-04-14 00:34:46 +0800

    完成了登录功能

--- src/main.js
+++ src/main.js
@@ -2,6 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+// 导入字体图标
+
+// 导入全局样式表
+import './assets/css/global.css'
+
+import axios from 'axios'
+// 为axios设置请求的根路径
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// 把axios这个包挂载到vue的原型对象上，这样的话vue的每一个组件都可以通过this获取到$http，从而发起ajax请求
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,9 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import { Button, Form, FormItem, Input, Message } from 'element-ui'
 
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+// message需要进行全局挂载，把弹框组件挂到了vue的原型对象上，这样的话每一个组件都可以通过$message来获取到弹框组件从而使用
+Vue.prototype.$message = Message
